331311a55067154596f9e282997dc6cf6a97ab4d
Vicente Jerez Fidalgo <vjfidalgo@x220.localdomain>
2018-07-03 21:08:18 -0400

    assignment #01

---
+++ LECTURE01/hello/hello.c
@@ -0,0 +1,23 @@
+/*
+  
+ First program of the course
+ 
+ Simple hello, World!
+ 
+ Created by vjfidalgo on 03-JUL-2018
+ 
+ gcc -g -O0 -Wall hello.c -o hello.o
+ 
+ */ 
+
+#include <stdio.h> /* basic I/O functionality */
+
+/* The main function -- entry point*/
+int main()
+{
+    const char myOtherString[] = "hello, people!\n";
+    
+    puts(myOtherString);
+    
+    return 0; /*returning 0 <=> success*/
+}
